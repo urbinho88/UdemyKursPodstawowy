5d2da57c06f91064c93129291dea3795722d83f9
Limak Nabru <urbinho88@users.noreply.github.com>
2020-12-29 22:15:04 +0100

    Wykonane!

---
+++ 20. Operatory Logiczne.py
@@ -0,0 +1,37 @@
+wartosc = int(input("Sprawdzę, czy liczba jest z zakresu od 1 do 10: "))
+
+
+if (wartosc > 1 and wartosc < 10):
+    print("wartosc jest od 1 do 10")
+
+'''
+a = 1
+b = 2
+if (not(a == 5 or b == 3)):
+    print("tak")
+'''
+
+'''
+OPERATORY LOGICZNE
+
+and - 'i'
+
+True True - True
+True False - False
+False True - False
+False False - False
+
+KONIUNKCJA jest prawdzia, wtedy i tylko wtedy, gdy OBA wyrażenia w tym samym MOMENCIE są prawdziwe
+
+or - 'lub'
+
+True True - True
+True False - True
+False True - True
+False False - False
+
+Alternatywa jest fałszywa, wtedy i tylko wtedy, gdy OBA wyrazenia są w tym samym momencie fałszywe!
+
+not - NIE
+
+'''
