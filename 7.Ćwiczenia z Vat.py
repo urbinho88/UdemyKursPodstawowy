3bc5b61407241ce208d7e1f0de150ae7541cf446
Limak Nabru <urbinho88@users.noreply.github.com>
2020-12-29 14:49:49 +0100

    Do wykonania

---
+++ 7.Ćwiczenia z Vat.py
@@ -0,0 +1,27 @@
+# Po każdej lekcji powinieneś wykonać to co zrobiłem w lekcji, aby nie zapomnieć nowo poznanych wiadomości.
+#
+# Dodatkowo poza tym potrenuj wykonując poniższe zadania:
+#
+# Ćw. 1
+#
+# Oblicz cenę brutto Kursu Java wiedząc, że:
+# cena netto kursu Java 35 zł
+# VAT - 21%
+# Ćw 2.
+#
+# Oblicz cenę brutto Kursu Ajax wiedząc, że :
+# cena netto kursu Ajax 25 zł
+# VAT - 20%
+# Ćw. 3
+#
+# Oblicz cenę netto oprogramowania wiedząc, że:
+# cena brutto oprogramowania 165 zł
+# VAT - 23%
+# Ćw. 4
+# Oblicz cenę netto programu wiedząc,że:
+# cena brutto programu wynosi 215 zł
+# VAT - 19 %
+#
+# Jeśli będziesz miał problem, pamiętaj, że do kursu dołączone są rozwiązania ćwiczeń.
+#
+# W razie ew. pytań wal śmiało :-) Pochwal się rozwiązaniem w sekcji: “pytania i odpowiedzi”
