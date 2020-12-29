c11c812cbbf736178ea94afec9dd024566708d55
Limak Nabru <urbinho88@users.noreply.github.com>
2020-12-29 14:31:26 +0100

    Wykonanie zadania poprawnie

---
+++ 6. Operatory artymetyczne (Matematyka w Pythonie).py
@@ -0,0 +1,10 @@
+# + dodawanie
+# - odejmowanie
+# * mnożenie
+# / dzielenie
+#
+# () - jak w matematyce kierują kolejnością działania
+#
+# ** potęgowanie
+# // dzielenie w "dół"
+# % - modulo (reszta z dzielenia)
