d8ad86dfe534daeca98de94487052fce0a890072
Limak Nabru <urbinho88@users.noreply.github.com>
2020-12-29 14:28:57 +0100

    Wykonanie zadania poprawnie

---
+++ 5. Typy zmiennych oraz nazewnictwo.py
@@ -0,0 +1,14 @@
+a = 5 #l. całkowita int (integer)
+
+b = 4.3 #l. zmiennoprzecinkową (float)
+
+zdanie = "I'm from Poland" #string - ciąg znaków
+
+print(zdanie)
+
+
+# czyDuzy = False #bool boolean
+#
+# A = 4
+#
+# rozmiarSekcjiGlownej = 5
