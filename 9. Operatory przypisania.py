aaaf52f5a44654ca92558a8855e77ca54316637d
Limak Nabru <urbinho88@users.noreply.github.com>
2020-12-29 15:23:07 +0100

    Wykonane

---
+++ 9. Operatory przypisania.py
@@ -0,0 +1,22 @@
+"""
+
+
++=	x += 5	to skrócony zapis: x = x + 5
+-=	x -= 5	to skrócony zapis: x = x - 5
+*=	x *= 5	to skrócony zapis: x = x * 5
+/=	x /= 5	to skrócony zapis: x = x / 5
+%=	x %= 5	to skrócony zapis: x = x % 5
+//=	x //= 5	to skrócony zapis: x = x // 5
+**=	x **= 5	to skrócony zapis: x = x ** 5
+
+
+"""
+
+
+x = 4
+
+#x = x + 3
+
+x *= 2
+
+print(x)
