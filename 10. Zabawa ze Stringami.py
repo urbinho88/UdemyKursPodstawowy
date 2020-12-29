894c0ee0a9b691773ecace6ee150e4b39f32f26f
Limak Nabru <urbinho88@users.noreply.github.com>
2020-12-29 17:24:50 +0100

    Wykonane

---
+++ 10. Zabawa ze Stringami.py
@@ -0,0 +1,14 @@
+imie = "Wiola"
+
+nazwisko = "Włodarczyk"
+
+pelne = imie + " " + nazwisko
+
+
+dlugiString = """To jest jakiś tekst o niczym
+To jest jakiś tekst o niczym To jest jakiś
+tekst o niczym To jest jakiś tekst o niczym
+To jest jakiś tekst o niczym To jest jakiś tekst o niczym """
+
+
+print(imie[-1])
